b285abe827c1390f94e324f29df498e489140d6e
東京科学集団2 <yojimasentaro@gmail.com>
2016-05-20 20:17:24 +0900

    write username, email, and password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  has_many :prototypes
+  devise    :database_authenticatable, :registerable,
+            :recoverable, :rememberable, :trackable, :validatable
+  has_many  :prototypes
+  validates :username,      presence: true
+  validates :password,   length: { minimum: 8}
+  validates :email,      presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 end
