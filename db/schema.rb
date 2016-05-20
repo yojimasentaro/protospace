39c4984087e4f339466b052ffa8e63a2d2e39137
東京科学集団2 <yojimasentaro@gmail.com>
2016-05-20 21:32:43 +0900

    add username column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160520072434) do
+ActiveRecord::Schema.define(version: 20160520122848) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160520072434) do
     t.text     "profile",                limit: 65535
     t.string   "member",                 limit: 255
     t.text     "works",                  limit: 65535
+    t.string   "username",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
