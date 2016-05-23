e23b4563d7147916f780366eeabd8a69d3e21321
東京科学集団2 <yojimasentaro@gmail.com>
2016-05-23 14:46:49 +0900

    show user page and edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_params)
+    redirect_to root_path
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(
+      :username,
+      :avatar,
+      :email,
+      :profile,
+      :member,
+      :profile,
+      :works
+      )
+  end
+
 end
