ef9613146383ed7354f212b608c59a3281e3c943
東京科学集団2 <yojimasentaro@gmail.com>
2016-05-25 01:39:40 +0900

    add columns to prototypes

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524161930) do
+ActiveRecord::Schema.define(version: 20160524163113) do
 
   create_table "prototypes", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "title",      limit: 255
+    t.string   "catch_copy", limit: 255
+    t.text     "concept",    limit: 65535
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
