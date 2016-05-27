927980cfb861caedd7128580ff2fdf32a636574f
東京科学集団2 <yojimasentaro@gmail.com>
2016-05-27 14:45:16 +0900

    nest relation user and prototype

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources  :users, only: [:show, :edit, :update] do
-    resources :prototypes
+    resources :prototypes do
+      resources :prototype_images
+    end
   end
 end
