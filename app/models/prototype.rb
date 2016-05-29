3e6fa2f82045b5ba90c7839652ae8f841b94ec19
東京科学集団2 <yojimasentaro@gmail.com>
2016-05-29 21:45:44 +0900

    enable allow_destroy

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many   :prototype_images
-  accepts_nested_attributes_for :prototype_images, alow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :prototype_images, allow_destroy: true, reject_if: :all_blank
   validates  :catch_copy, :concept, :title,  presence: true
 end
