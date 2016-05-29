4a52b3c49fd9732696f88240020b66c128741351
東京科学集団2 <yojimasentaro@gmail.com>
2016-05-29 23:12:24 +0900

    cancel nest between user and prototype

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources  :users, only: [:show, :edit, :update] do
-    resources :prototypes
-  end
+  resources  :users, only: [:show, :edit, :update]
+  resources :prototypes
 end
