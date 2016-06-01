5f8eca8e5db60c07bab32cfbf79481ea13c5ed43
東京科学集団2 <yojimasentaro@gmail.com>
2016-06-02 04:34:27 +0900

    destroy prototype_images in deleting prototype

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many   :prototype_images
+  has_many   :prototype_images, dependent: :destroy
   has_one    :main, class_name: "PtototypeImage"
   accepts_nested_attributes_for :prototype_images, allow_destroy: true, reject_if: :all_blank
   validates  :catch_copy, :concept, :title,  presence: true
