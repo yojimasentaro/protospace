394c4df94f060d7d46699e7e3ebc6ed7fb480ff5
東京科学集団2 <yojimasentaro@gmail.com>
2016-06-08 23:53:14 +0900

    integrate js related to action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,16 @@
 class LikesController < ApplicationController
 
   before_action :set_prototype, :set_likes, only: [:create, :destroy]
+
   def create
     @like  = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    render partial: "likes/js_like"
   end
 
   def destroy
     @like = current_user.likes.find_by(prototype_id: params[:prototype_id])
     @like.destroy
+    render partial: "likes/js_like"
   end
 
   private
